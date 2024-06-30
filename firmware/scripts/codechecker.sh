686578bcbce791353217d705a22145e46c6627da
Saku Glumoff <saku.glumoff@gmail.com>
2024-06-30 19:22:47 +0300

    Replace Jenkins with GitHub actions

---
+++ firmware/scripts/codechecker.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+FILES_TO_CHECK=$(
+	find . \
+	-type f \
+	-not -path './drivers/include/stm32l552xx.h' \
+	-not -path './lib*' \
+	-not -path './build*' \
+	\( -name '*.h' -or -name '*.hpp' -or -name '*.c' -or -name '*.cpp' \)
+)
+
+for FILE_TO_CHECK in $FILES_TO_CHECK
+do
+	echo "Checking $FILE_TO_CHECK..."
+	clang-format $FILE_TO_CHECK --dry-run --Werror
+	ERR=$?
+	if [ $ERR -ne 0 ]; then
+		echo "$FILE_TO_CHECK not formatted properly!"
+		break
+	fi
+done
+
+exit $ERR
