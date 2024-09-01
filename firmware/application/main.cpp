f926faece30ff652745c86bf93303a34eae74509
Saku Glumoff <saku.glumoff@gmail.com>
2024-09-01 10:49:37 +0000

    Remove PTSW, update DebugPrint-GCC, add FreeRTOS

--- firmware/application/main.cpp
+++ firmware/application/main.cpp
@@ -1,3 +1,4 @@
+#include "FreeRTOS.h"
 #include "SEGGER_RTT.h"
 #include "common.hpp"
 #include "config.hpp"
@@ -5,6 +6,7 @@
 #include "gpio.hpp"
 #include "pinmap.hpp"
 #include "system.hpp"
+#include "task.h"
 
 extern "C" int debug_print_callback(char* debugMessage, unsigned int length) {
 	unsigned avail = SEGGER_RTT_GetAvailWriteSpace(0);
@@ -14,12 +16,44 @@ extern "C" int debug_print_callback(char* debugMessage, unsigned int length) {
 	return SEGGER_RTT_Write(0, debugMessage, length);
 }
 
+void vApplicationStackOverflowHook(TaskHandle_t xTask, char* pcTaskName) {
+	(void)xTask;
+	DBG_PRINTF_ERROR("Task stack overflow: %s", pcTaskName);
+	while (true) {}
+}
+
 /**
  * @brief Initialize system and peripherals.
  */
 static void _Init() {
 	SystemInit();
 	SEGGER_RTT_Init();
+}
+
+static constexpr uint32_t LED_TASK_STACK_SIZE = 1'000UL;
+static StackType_t        _ledTaskStack[LED_TASK_STACK_SIZE];
+static StaticTask_t       _ledTaskBuffer;
+
+static void _ledTask(void* arg) {
+	(void)arg;
+	Gpio led(
+		LED_DBG_A,
+		Gpio::Mode::OUTPUT,
+		Gpio::Type::PUSH_PULL,
+		Gpio::PullUp::PULL_UP,
+		Gpio::Speed::LOW
+	);
+	led = 1;
+
+	while (true) {
+		if (led) {
+			DBG_PRINTF_TRACE("Setting LED on");
+		} else {
+			DBG_PRINTF_TRACE("Setting LED off");
+		}
+		led = !led;
+		vTaskDelay(pdMS_TO_TICKS(500));
+	}
 }
 
 int main(void) {
@@ -32,26 +66,22 @@ int main(void) {
 	DBG_PRINTF_TRACE("Timestamp: %s", header->metadata.timestamp);
 	DBG_PRINTF_TRACE("Commit: %s", header->metadata.commit);
 	DBG_PRINTF_TRACE("Toolchain: %s", header->metadata.toolchain);
+	Sleep(msToTicks(500));
 
-	bool ledState = true;
-	Gpio led(
-		LED_DBG_A,
-		Gpio::Mode::OUTPUT,
-		Gpio::Type::PUSH_PULL,
-		Gpio::PullUp::PULL_UP,
-		Gpio::Speed::LOW
+	xTaskCreateStatic(
+		_ledTask,
+		"LED",
+		LED_TASK_STACK_SIZE,
+		nullptr,
+		tskIDLE_PRIORITY,
+		_ledTaskStack,
+		&_ledTaskBuffer
 	);
-	led = 1;
+
+	vTaskStartScheduler();
 
 	while (true) {
-		if (ledState) {
-			DBG_PRINTF_TRACE("Setting LED on");
-		} else {
-			DBG_PRINTF_TRACE("Setting LED off");
-		}
-		led      = ledState;
-		ledState = !ledState;
-		Sleep(msToTicks(500));
+		__NOP();
 	}
 
 	return 0;
